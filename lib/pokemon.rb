9dfce35d57e050d3a1d6f65277e0e0ef4ae44c71
Karl Amoguis <ktamoguis@gmail.com>
2018-06-02 07:41:23 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -27,7 +27,6 @@ class Pokemon
   def self.find(id, db)
     #@@all.detect { |element| element = db.execute("SELECT * FROM pokemon where id = #{id}") }
     #newobject = Pokemon.new
-    array = []
     hash = {}
     array = db.execute("SELECT * FROM pokemon where id = #{id}")
     hash[:id] = array[0][0]
